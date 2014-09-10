f81f5c07bc43bc806483313cb876905e471ae314
CapacitorSet <arcadiagiulio@gmail.com>
2014-09-10 23:00:47 +0200

    Implemented textMode, made a couple of bugfixes

--- make.js
+++ make.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
 
-// Insert here additional code to deal with keys.
-// Notice how "\" is used to continue the string on a new line.
-
-additionalCode = "";
-
 fs = require('fs');
 keys = JSON.parse(fs.readFileSync('keys.json').toString('utf8'));
 originalProgram = fs.readFileSync('Base.raw').toString('utf8');
-
-replacement = "\n"
+replacement = "\n";
 
 // All the \ts don't serve any real function and are ignored by the compiler, but they make the code more readable (thus making debugging easier).
 
@@ -40,14 +34,28 @@ for (key in keys) {
 		replacement += "\t\tlatestExpressionLength = " + data.shift.toExpression.length + ";\n";
 		replacement += "\t} else\n";
 	}
-	if (data.vanilla) {
-		replacement += "\t if (!ctrl && !shift) {\n"
-		replacement += "\t\tbuffer     += '" + data.vanilla.toBuffer + "';\n";
-		if (!data.vanilla.toExpression) { data.vanilla.toExpression = data.vanilla.toBuffer; } // default the toExpression property to the value of toBuffer
-		replacement += "\t\texpression += \"" + data.vanilla.toExpression + "\";\n";
-		replacement += "\t\tlatestExpressionLength = " + data.vanilla.toExpression.length + ";\n";
-		replacement += "\t}\n"
-	}
+	replacement += "\tif (!ctrl && !shift) {\n"
+	replacement += "\t\tif (textMode) {\n";
+	textModeChoices = data.textMode.length;
+	replacement += "\t\t\ttapCount = tapCount % " + textModeChoices + ";\n";
+	replacement += "\t\t\tswitch (tapCount) {\n"
+	tapCount = 0;
+	data.textMode.forEach(function (key) {
+		replacement += "\t\t\t\tcase " + tapCount + ":\n";
+		replacement += "\t\t\t\t\tbuffer     += \"" + key + "\";\n";
+		replacement += "\t\t\t\t\texpression += \"" + key + "\";\n";
+		replacement += "\t\t\t\t\tlatestExpressionLength = 1;\n";
+		replacement += "\t\t\t\t\tbreak;\n"
+		tapCount++;
+	});
+	replacement += "\t\t\t}\n";
+	replacement += "\t\t} else {\n";
+	replacement += "\t\t\tbuffer     += '" + data.vanilla.toBuffer + "';\n";
+	if (!data.vanilla.toExpression) { data.vanilla.toExpression = data.vanilla.toBuffer; } // default the toExpression property to the value of toBuffer
+	replacement += "\t\t\texpression += \"" + data.vanilla.toExpression + "\";\n";
+	replacement += "\t\t\tlatestExpressionLength = " + data.vanilla.toExpression.length + ";\n";
+	replacement += "\t\t}\n";
+	replacement += "\t}\n";
 
 	replacement += "\tbreak;\n"
 }
