509f9f7aa1ac7aa6a03e6b1a44423fd7eba58b73
CapacitorSet <arcadiagiulio@gmail.com>
2014-09-10 17:35:21 +0200

    Moved to 'make' for the build process; redesigned keypress management in a more compact way (you can control what keys print what in keys.json, and define additional behaviour in Base.raw).

--- make.js
+++ make.js
@@ -6,30 +6,52 @@
 additionalCode = "";
 
 fs = require('fs');
-replacements = JSON.parse(fs.readFileSync('keys.json').toString('utf8'));
+keys = JSON.parse(fs.readFileSync('keys.json').toString('utf8'));
 originalProgram = fs.readFileSync('Base.raw').toString('utf8');
 
-for (textToBeReplaced in replacements) {
-	keys = replacements[textToBeReplaced];
+replacement = "\n"
 
-	replacement = "\n"
+// All the \ts don't serve any real function and are ignored by the compiler, but they make the code more readable (thus making debugging easier).
 
-	// All the \ts don't serve any real function and are ignored by the compiler, but they make the code more readable (thus making debugging easier).
-
-	for (key in keys) {
-		chars = keys[key]; // Eg. if keys = {"1": ["a", "b". "c", "1"]}, then key = "1" and "chars" = ["a", "b". "c", "1"]
-		replacement += "\tcase '" + key + "':\n";
-		replacement += '\t\tclearScreen = true;\n\t\tvaScritto = true;\n\t\tswitch (tapCount % ' + chars.length + ') {\n';
-		tapCount = 0;
-		chars.forEach(function (char) {
-			replacement += '\t\t\tcase ' + tapCount + ':\n';
-			replacement += "\t\t\t\tkey = '" + char + "';\n\t\t\t\tbreak;\n";
-			tapCount++;
-		});
-		replacement += "\t\t}\n\t\tespressione += key;\n\t\tbreak;\n";
+for (key in keys) {
+	data = keys[key]
+	replacement += "case '" + key + "':\n";
+	if (data.ctrlShift) {
+		replacement += "\tif (ctrl && shift) {\n"
+		replacement += "\t\tbuffer     += '" + data.ctrlShift.toBuffer + "';\n";
+		if (!data.ctrlShift.toExpression) { data.ctrlShift.toExpression = data.ctrlShift.toBuffer; } // default the toExpression property to the value of toBuffer
+		replacement += "\t\texpression += \"" + data.ctrlShift.toExpression + "\";\n";
+		replacement += "\t\tlatestExpressionLength = " + data.ctrlShift.toExpression.length + ";\n";
+		replacement += "\t} else\n";
+	}
+	if (data.ctrl) {
+		replacement += "\tif (ctrl && !shift) {\n"
+		replacement += "\t\tbuffer     += '" + data.ctrl.toBuffer + "';\n";
+		if (!data.ctrl.toExpression) { data.ctrl.toExpression = data.ctrl.toBuffer; } // default the toExpression property to the value of toBuffer
+		replacement += "\t\texpression += \"" + data.ctrl.toExpression + "\";\n";
+		replacement += "\t\tlatestExpressionLength = " + data.ctrl.toExpression.length + ";\n";
+		replacement += "\t} else\n";
+	}
+	if (data.shift) {
+		replacement += "\tif (!ctrl && shift) {\n"
+		replacement += "\t\tbuffer     += '" + data.shift.toBuffer + "';\n";
+		if (!data.shift.toExpression) { data.shift.toExpression = data.shift.toBuffer; } // default the toExpression property to the value of toBuffer
+		replacement += "\t\texpression += \"" + data.shift.toExpression + "\";\n";
+		replacement += "\t\tlatestExpressionLength = " + data.shift.toExpression.length + ";\n";
+		replacement += "\t} else\n";
+	}
+	if (data.vanilla) {
+		replacement += "\t if (!ctrl && !shift) {\n"
+		replacement += "\t\tbuffer     += '" + data.vanilla.toBuffer + "';\n";
+		if (!data.vanilla.toExpression) { data.vanilla.toExpression = data.vanilla.toBuffer; } // default the toExpression property to the value of toBuffer
+		replacement += "\t\texpression += \"" + data.vanilla.toExpression + "\";\n";
+		replacement += "\t\tlatestExpressionLength = " + data.vanilla.toExpression.length + ";\n";
+		replacement += "\t}\n"
 	}
 
-	originalProgram = originalProgram.replace("/* " + textToBeReplaced + " */", replacement);
-
+	replacement += "\tbreak;\n"
 }
+
+originalProgram = originalProgram.replace("/* KEYS */", replacement);
+
 fs.writeFileSync("calculator.ino", originalProgram);
